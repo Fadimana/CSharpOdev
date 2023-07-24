1b3ad175132f92bfbfa5744b5e095045fa053c70
Fadimana <fdmndikici@gmail.com>
2023-07-24 20:48:26 +0300

    .

---
+++ Ödev/Ödev/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+            foreach(int sayilar in Numbers)
+            {
+                Console.WriteLine("Sayilar:{0}", sayilar);
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
